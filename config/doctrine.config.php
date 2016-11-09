ca759c01d92a0a55e4a22706c4b37af5debf9619
Ainias <silasg@web.de>
2016-11-09 11:40:05 +0100

    kurzes Update

--- config/doctrine.config.php
+++ config/doctrine.config.php
@@ -2,17 +2,10 @@
 
 namespace Ainias\Core;
 
-use Ainias\Core\Connections\MyConnection;
 use Doctrine\ORM\Mapping\Driver\AnnotationDriver;
 
 return array(
     'doctrine' => array(
-        'connection' => array(
-            'default' => array(
-                'wrapperClass' => MyConnection::class,
-                'params' => [],
-            )
-        ),
         'driver' => array(
             'orm_default' => array(
                 'drivers' => array(
