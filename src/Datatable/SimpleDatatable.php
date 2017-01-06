b9b11c89c37d9cb84625d9db12e5c9363592ba34
silas <silas42897@t-online.de>
2017-01-06 16:04:20 +0100

    kurzes Datatable-Update

---
+++ src/Datatable/SimpleDatatable.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: silas
+ * Date: 06.01.17
+ * Time: 15:52
+ */
+
+namespace Ainias\Core\Datatable;
+
+class SimpleDatatable extends Datatable
+{
+    public function __construct($id, array $options = array())
+    {
+        $this->setPaginate(false);
+        $this->setSortable(false);
+        parent::__construct($id, $options);
+    }
+
+    protected function buildJsInitObject()
+    {
+        $pageLength = $this->getPageLength();
+        $lengthMenu = $this->getLengthMenu();
+        $lengthMenu = "[[" . implode(", ", $lengthMenu) . "], [\"" . implode("\",\"", array_keys($lengthMenu)) . "\"]]";
+        $paginate = ($this->isPaginate()) ? "true" : "false";
+        $responsive = "false";
+        if ($this->isResponsive()) {
+            $renderer = $this->getResponsiveRenderer();
+            $responsive =
+                <<<JS
+                {
+                details: {
+                    renderer: $renderer
+                }
+            }
+JS;
+        }
+
+        $scriptColumns = $this->buildJsColumns();
+
+        $script = <<<JS
+		var dataTableInitObject = {
+					oLanguage :{
+				sEmptyTable: "<b>Keine Daten vorhanden.</b>",
+				sInfoEmpty: "Keine Einträge vorhanden",
+				sInfoFiltered: "(gefiltert von _MAX_ Einträgen)",
+				sInfoThousands: ".",
+				sLengthMenu: "Zeige _MENU_ Einträge",
+				sZeroRecords: "Keine Einträge gefunden",
+				sSearch: "Suche:",
+				emptyTable: "Keine Daten vorhanden",
+				oPaginate: {
+					sNext: "Nächste Seite",
+					sPrevious: "Vorherige Seite"
+				}
+			},
+			bFilter: false,
+			bInfo: false,
+			responsive: $responsive,
+			 dom: '<"top"lpf<"clear">>rt<"bottom"ip<"clear">>'
+					};
+					dataTableInitObject.pageLength = $pageLength;
+		dataTableInitObject.lengthMenu = $lengthMenu;
+		dataTableInitObject.bPaginate = $paginate;
+		dataTableInitObject.aoColumnDefs = [$scriptColumns];
+JS;
+        return $script;
+    }
+}
